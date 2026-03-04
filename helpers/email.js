d564d4b4f23674481e8d47c8ced078d0a2c57429
antoniovga1965-glitch <antoniovga1965@gmail.com>
2026-03-04 19:19:32 +0300

    fix email port

--- helpers/email.js
+++ helpers/email.js
@@ -1,6 +1,9 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service:'gmail',
+    host:'smtp.gmail.com',
+     port: 587,
+  secure: false,
     auth:{
         user:process.env.EMAILUSER,
         pass:process.env.EMAIL_PASS
